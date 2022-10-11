c9fd5bf41030d0c8873b6d9695ae733235b4bcc3
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-11 14:37:48 -0300

    Created the Card component to group the other components created previously

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,30 @@ import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/Fragmento';
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
+
 export default (props) => {
     return (
         <div id="app">
             <h1>Fundamentos React</h1>
 
-            <Aleatorio 
-                min="1"
-                max="100"
-            />
+            <Card titulo="#04 Desafio Aleatório">
+                <Aleatorio min={1} max={60} />
+            </Card>
+            <Card titulo="#03 ragmento">
+                <Fragmento />
+            </Card>
+            <Card titulo="#02 Com Parâmetro">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro"
+                    nota={9.3}
+                />
+            </Card>
+            <Card titulo="#01 Primeiro Componente">
+                <Primeiro />
+            </Card>
 
-            <Fragmento />
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Pedro"
-                nota={9.3}
-            />
-            <Primeiro></Primeiro>
 
         </div>
     )
