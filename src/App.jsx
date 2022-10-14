2dbea80c68b73927ea5cc811c6b815069fde6c6d
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-14 13:33:00 -0300

    Using useState to set the data in the component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/Fragmento';
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
-import ParOuImpar from './components/condicional/ParOuImpar.tsx';
+import ParOuImpar from './components/condicional/ParOuImpar';
 import Familia from './components/basicos/Familia';
 import FamiliaMembro from './components/basicos/FamiliaMembro';
 import ListaAlunos from './components/repeticao/ListaAlunos';
 import TabelaProdutos from './components/repeticao/TabelaProdutos';
+import UsuarioInfo from './components/condicional/Usuarioinfo';
+import DiretaPai from './components/comunicacao/DiretaPai';
+import IndiretaPai from './components/comunicacao/IndiretaPai';
 
 
 export default () => {
@@ -18,8 +21,15 @@ export default () => {
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
+            <Card titulo="#10 Comunicação Indireta" color="#8BAD39">
+                <IndiretaPai />
+            </Card>
+            <Card titulo="#09 Comunicação Direta" color="#59323C">
+                <DiretaPai />
+            </Card>
             <Card titulo="#08 Renderização Condicional" color="#982395">
                 <ParOuImpar numero={20}></ParOuImpar>
+                <UsuarioInfo usuario={{ nome: 'Fernando' }} />
             </Card>
             <Card titulo="#07 Desafio Repetição" color="#FF432E">
                 <TabelaProdutos />
